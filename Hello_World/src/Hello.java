de3298956096487bb564a99647839912dbba01c7
Alan Schambers <aschambers4291@gmail.com>
2017-08-17 14:57:36 -0700

    integers

--- Hello_World/src/Hello.java
+++ Hello_World/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFirstNumber = 10 + 5;
+        int mySecondNumber = 12 * 3;
+        int myThirdNumber = 73;
+        System.out.println("first: " + myFirstNumber + " second: " + mySecondNumber + " third: " + myThirdNumber);
     }
 }
